b6d9acdfaccad77f2530e7b3571b325fc18a3c86
Brando Rocha <brandorocha00@gmail.com>
2026-01-17 10:01:17 -0300

    feat: added more type to domain

--- encoder/domain/job.go
+++ encoder/domain/job.go
@@ -7,6 +7,7 @@ type Job struct {
   OutputBucketPath  string
   Status            string
   Video             *Video
+	VideoID						string
   Error             string
   CreatedAt         time.Time
   UpdatedAt         time.Time
